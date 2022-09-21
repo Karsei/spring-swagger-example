d1d0a14bdf7c01ee604e50bc706706f9209c786a
Karsei <karseidev@gmail.com>
2022-09-21 23:46:45 +0900

    feat: added swagger config to controller

--- src/main/java/kr/pe/karsei/springswaggerexample/SampleController.java
+++ src/main/java/kr/pe/karsei/springswaggerexample/SampleController.java
@@ -1,6 +1,8 @@
 package kr.pe.karsei.springswaggerexample;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/sample")
 public class SampleController {
     @GetMapping("/{id}")
-    @Operation(summary = "문자열을 그대로 출력")
+    @Operation(summary = "문자열을 그대로 출력", description = "문자열을 그대로 출력합니다.", tags = { "태애애애그", "예아" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "내부 서버 오류")
+    })
     public String hello(@PathVariable String id) {
         return id;
     }
